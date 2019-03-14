689fc5b6b2bcbd0793bc78f9b0f51d2d726c1d9f
liujiajun <liujiajun@qq.com>
2019-03-14 23:24:07 +0800

    增加区间和原生集合

---
+++ src/main/java/xin/liujiajun/guava/range/RangeDemo.java
@@ -0,0 +1,34 @@
+package xin.liujiajun.guava.range;
+
+import com.google.common.collect.Range;
+
+/**
+ * @author LiuJiaJun
+ * @date 2019/3/14 22:27
+ */
+public class RangeDemo {
+
+    public static void main(String[] args) {
+
+        //构造区间
+        //(1..2)
+        Range<Integer> open = Range.open(1, 2);
+        //[1..2]
+        Range<Integer> closed = Range.closed(1, 2);
+        //[1,2)
+        Range<Integer> integerRange = Range.closedOpen(1, 2);
+        //(1,2]
+        Range<Integer> integerRange1 = Range.openClosed(1, 2);
+        // (1..+∞)
+        Range<Integer> integerRange2 = Range.greaterThan(1);
+        //[1..+∞)
+        Range<Integer> integerRange3 = Range.atLeast(1);
+        //(-∞..1)
+        Range<Integer> integerRange4 = Range.lessThan(1);
+        //(-∞..1]
+        Range<Integer> integerRange5 = Range.atMost(1);
+        //(-∞..+∞)
+        Range<Comparable<?>> all = Range.all();
+
+    }
+}
