ed041adcc0db227ee5d40a57c512d20010dd04d7
刘佳俊 <liujj@yealink.com>
2019-03-15 18:18:43 +0800

    增加并发和图形demo

---
+++ src/main/java/xin/liujiajun/guava/concurrency/AbstractIdleServiceDemo.java
@@ -0,0 +1,33 @@
+package xin.liujiajun.guava.concurrency;
+
+import com.google.common.util.concurrent.AbstractIdleService;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author liujiajun
+ * @description
+ * @create 2019-03-15 14:32
+ **/
+public class AbstractIdleServiceDemo extends AbstractIdleService {
+
+
+    @Override
+    protected void startUp() throws Exception {
+        System.out.println("启动");
+    }
+
+    @Override
+    protected void shutDown() throws Exception {
+        System.out.println("关闭");
+    }
+
+
+
+    public static void main(String[] args) {
+        AbstractIdleServiceDemo demo = new AbstractIdleServiceDemo();
+        demo.startAsync();
+    }
+}
